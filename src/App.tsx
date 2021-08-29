717d5209a7ba8907741fd1391fdfd52010bfc418
Anucha M <monopeelz@gmail.com>
2021-08-29 22:17:47 +0700

    add delete functions

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,17 @@ function App() {
     e.preventDefault()
     setTasks([...tasks, title])
     setTitle("")
-    return
+  }
+  function deleteTask(i: number) {
+    let nTasks = [...tasks]
+    nTasks.splice(i, 1)
+    setTasks(nTasks)
   }
 
   return (
     <div>
       <ul>
-        {tasks.map(item => <li>{item}</li>)}
+        {tasks.map((item, i) => <li>{item} <span onClick={() => deleteTask(i)}>X</span></li>)}
       </ul>
       <form onSubmit={addTask}>
         <label >Title:</label>
